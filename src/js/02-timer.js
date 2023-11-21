c0fd06903afbe8694740ff1c2fe3fa61aa76709d
gamp300 <49374048+gamp300@users.noreply.github.com>
2023-11-21 13:33:29 -0500

    Update T09 .30

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,78 +2,70 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    selectedDate = selectedDates[0];
-
-    if (selectedDate < new Date()) {
-      Notiflix.Notify.failure('Elija una fecha en el futuro');
-      document.querySelector('[data-start]').disabled = true;
-    } else {
-      document.querySelector('[data-start]').disabled = false;
-    }
-  },
-};
-
-flatpickr('#datetime-picker', options);
-
-let idInterval;
-let dateEnd;
-let selectedDate;
-
 function startTimer() {
-  // // dateEnd = flatpickr.parseDate(
-  //   document.querySelector('#datetime-picker').value
-  // );
-  const defauDate = new Date();
-  dateEnd = selectedDate.getTime() - defauDate.getTime();
-  const transMs = convertMs(dateEnd);
+  const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      handleDateSelection(selectedDates[0]);
+    },
+  };
 
-  console.log(transMs);
-  idInterval = setInterval(updateTimer, 1000);
+  flatpickr('#datetime-picker', options);
+}
+
+function handleDateSelection(selectedDate) {
+  const now = new Date();
+
+  if (selectedDate <= now) {
+    Notiflix.Notify.failure('Elija una fecha en el futuro.');
+    document.querySelector('[data-start]').disabled = true;
+  } else {
+    document.querySelector('[data-start]').disabled = false;
+    startCountdown(selectedDate);
+  }
+}
+
+function startCountdown(endDate) {
+  const timerInterval = setInterval(updateTimer, 1000, endDate);
+
+  updateTimer(endDate);
 
   document.querySelector('[data-start]').disabled = true;
+
+  function updateTimer(endDate) {
+    const now = new Date();
+    const timeDifference = endDate - now;
+
+    if (timeDifference <= 0) {
+      clearInterval(timerInterval);
+      Notiflix.Notify.success('Se completo el tiempo!');
+      document.querySelector('[data-start]').disabled = false; // Habilitar el botón nuevamente
+    } else {
+      const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor(
+        (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
+      );
+      const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+      document.querySelector('[data-days]').textContent = formatTimeUnit(days);
+      document.querySelector('[data-hours]').textContent =
+        formatTimeUnit(hours);
+      document.querySelector('[data-minutes]').textContent =
+        formatTimeUnit(minutes);
+      document.querySelector('[data-seconds]').textContent =
+        formatTimeUnit(seconds);
+    }
+  }
+
+  function formatTimeUnit(unit) {
+    return unit < 10 ? '0' + unit : unit;
+  }
 }
 
-// function updateTimer() {
-//   const now = new Date();
-//   const timeDifference = dateEnd - now;
-
-//   if (timeDifference <= 0) {
-//     clearInterval(idInterval);
-//     Notiflix.Notify.success('Se completo el tiempo!');
-//   } else {
-//     const { days, hours, minutes, seconds } = convertMs(timeDifference);
-//     renderTimer({ days, hours, minutes, seconds });
-//   }
-// }
-
-function convertMs(ms) {
-  const second = 1000;
-  const minute = 60 * second;
-  const hour = 60 * minute;
-  const day = 24 * hour;
-
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor((ms % hour) / minute);
-  const seconds = Math.floor((ms % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
-
-function addLeading(value) {
-  return value.toString().padStart(2, '0');
-}
-
-function renderTimer({ days, hours, minutes, seconds }) {
-  document.querySelector('[data-days]').textContent = addLeading(days);
-  document.querySelector('[data-hours]').textContent = addLeading(hours);
-  document.querySelector('[data-minutes]').textContent = addLeading(minutes);
-  document.querySelector('[data-seconds]').textContent = addLeading(seconds);
-}
-document.querySelector('[data-start]').addEventListener('click', startTimer);
+document.addEventListener('DOMContentLoaded', startTimer);
